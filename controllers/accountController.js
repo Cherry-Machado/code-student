c2d7539de12e61518b44474a5126097e7b9b9421
Cherry-Machado <Cherry5467@gmail.com>
2025-03-29 20:52:38 -0300

    Creating the account/login.ejs file in view folder

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,9 +1,14 @@
 const utilities = require("../utilities/");
-const baseController = {};
 
-baseController.buildHome = async function (req, res) {
-  const nav = await utilities.getNav();
-  res.render("index", { title: "Home", nav });
-};
+/* ****************************************
+*  Deliver login view
+* *************************************** */
+async function buildLogin(req, res, next) {
+  let nav = await utilities.getNav()
+  res.render("account/login", {
+    title: "Login",
+    nav,
+  })
+}
 
-module.exports = baseController;
+module.exports = { buildLogin }
