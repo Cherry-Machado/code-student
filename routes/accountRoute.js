c7110ad8e64a8f7f2841699815bc2f8f526cfa92
Cherry-Machado <Cherry5467@gmail.com>
2025-03-29 23:08:24 -0300

    Updating

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -10,28 +10,4 @@ const utilities = require("../utilities");
 // Route to build Login View
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
 
-// Route to build registration view
-router.get(
-  "/register",
-  utilities.handleErrors(accountController.buildRegister)
-);
-
-// Process Management view
-router.get(
-  "/",
-  utilities.checkLogin,
-  utilities.handleErrors(accountController.buildManagement)
-);
-
-router.get(
-  "/update-account/:accountId",
-  utilities.checkLogin,
-  utilities.handleErrors(accountController.buildAccountUpdate)
-);
-
-router.get(
-  "/logout",
-  utilities.handleErrors(accountController.buildLogoutView)
-);
-
 module.exports = router;
