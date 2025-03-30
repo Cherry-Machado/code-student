3c546014450466af6049eb01b5345ba6509d6ae0
Cherry-Machado <Cherry5467@gmail.com>
2025-03-30 00:34:14 -0300

    updating

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,11 +1,11 @@
-const utilities = require("/utilities/");
+const utilities = require("../utilities/");
 
 /* ****************************************
  *  Deliver login view
  * *************************************** */
 async function buildLogin(req, res, next) {
   let nav = await utilities.getNav();
-  res.render("account/login", {
+  res.render("/account/login", {
     title: "Login",
     nav,
   });
