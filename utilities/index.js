bea8ce11b885e4ea4df617f48eae3561038eea71
Cherry-Machado <Cherry5467@gmail.com>
2025-04-01 22:01:47 -0300

    BuildingClassification_list in utilities

--- utilities/index.js
+++ utilities/index.js
@@ -1,6 +1,8 @@
 const invModel = require("../models/inventory-model");
 const Util = {};
 const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
 /* ************************
  * Constructs the nav HTML unordered list
  ************************** */
@@ -85,5 +87,30 @@ Util.buildClassificationGrid = async function (data) {
  **************************************** */
 Util.handleErrors = (fn) => (req, res, next) =>
   Promise.resolve(fn(req, res, next)).catch(next);
+
+
+/* ****************************************
+ * Build an HTML select element with
+ * Wclassification data
+ **************************************** */
+ 
+Util.buildClassificationList = async function (classification_id = null) {
+  let data = await invModel.getClassifications();
+  let classificationList =
+    '<select name="classification_id" id="classificationList" required>';
+  classificationList += "<option value=''>Choose a Classification</option>";
+  data.rows.forEach((row) => {
+    classificationList += '<option value="' + row.classification_id + '"';
+    if (
+      classification_id != null &&
+      row.classification_id == classification_id
+    ) {
+      classificationList += " selected ";
+    }
+    classificationList += ">" + row.classification_name + "</option>";
+  });
+  classificationList += "</select>";
+  return classificationList;
+};
 
 module.exports = Util;
