89f2d60e1850ce721ed7c16b7e722f95dbf7e354
Cherry-Machado <Cherry5467@gmail.com>
2025-04-15 21:12:11 -0300

    Updating

--- public/js/inventory.js
+++ public/js/inventory.js
@@ -6,6 +6,7 @@ classificationList.addEventListener("change", function () {
   let classification_id = classificationList.value;
   console.log(`classification_id is: ${classification_id}`);
   let classIdURL = "/inv/getInventory/" + classification_id;
+  console.log(classIdURL);
   fetch(classIdURL)
     .then(function (response) {
       if (response.ok) {
