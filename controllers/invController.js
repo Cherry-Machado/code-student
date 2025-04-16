5515955d7c55a5298818c589c438d97d9f4f43cf
Cherry-Machado <Cherry5467@gmail.com>
2025-04-15 21:20:32 -0300

    Updating

--- controllers/invController.js
+++ controllers/invController.js
@@ -172,7 +172,7 @@ invCont.getInventoryJSON = async (req, res, next) => {
   const invData = await invModel.getInventoryByClassificationId(
     classification_id
   );
-  if (invData[0] === classification_id) {
+  if (invData[0].inv_id) {
     return res.json(invData);
   } else {
     next(new Error("No data returned"));
